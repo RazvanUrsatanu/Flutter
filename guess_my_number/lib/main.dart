ebdf00640d15c0b7fce895f98b43eaaccdb1e71d
Razvan <razvan.ursatanu@yahoo.com>
2021-07-07 13:47:54 +0300

    add analysis_options

--- guess_my_number/lib/main.dart
+++ guess_my_number/lib/main.dart
@@ -3,12 +3,13 @@ import 'dart:math';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -16,13 +17,13 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  const MyHomePage({Key key, this.title}) : super(key: key);
   final String title;
 
   @override
@@ -34,115 +35,124 @@ class _MyHomePageState extends State<MyHomePage> {
   int aux;
   String wrong;
   String button = 'Guess';
-  final textControler = TextEditingController();
-  static Random ran = new Random();
+  final TextEditingController textControler = TextEditingController();
+  static Random ran = Random();
   int randomNumber = ran.nextInt(100) + 1;
 
-  void guess() {
-
-  }
+  void guess() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Guess my number"),
+        title: const Text('Guess my number'),
         centerTitle: true,
       ),
-      body: Container (
-        child: Column (
-          children: [
-            Text("I'm thinking of a number between"),
-            Text("1 to 100"),
-            Text("It's your turn to guess my number"),
-            if(wrong != null && number != -1)
-              Text(
+      body: Column(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              const Text('I am thinking of a number between'),
+              const Text('1 to 100'),
+              const Text('It is your turn to guess my number'),
+              if (wrong != null && number != -1)
+                Text(
                   wrong,
                   textScaleFactor: 2,
-              ),
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Material(
-                elevation: 50.00,
-                shadowColor: Colors.purple,
-                child: TextField(
-                  controller: textControler,
-                  decoration: InputDecoration(
-                    fillColor: Colors.white,
-                    filled: true,
-                  ),
-                    onChanged: (String x) {
-                      number = int.parse(x);
-                    }
+                ),
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Material(
+                  elevation: 50.00,
+                  shadowColor: Colors.purple,
+                  child: TextField(
+                      controller: textControler,
+                      decoration: const InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                      ),
+                      onChanged: (String x) {
+                        number = int.parse(x);
+                      }),
                 ),
               ),
-            ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                ElevatedButton(
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  ElevatedButton(
                     child: Text(button),
                     onPressed: () {
-                      setState(() {
-                        button = 'Guess';
-                      },);
+                      setState(
+                        () {
+                          button = 'Guess';
+                        },
+                      );
                       if (number > randomNumber) {
-                        setState(() {
-                          wrong = 'You tried ' + number.toString() + ', Try lower';
-                        },);
+                        setState(
+                          () {
+                            wrong = 'You tried ' + number.toString() + ', Try lower';
+                          },
+                        );
                       } else if (number < randomNumber) {
-                        setState(() {
-                          wrong = 'You tried ' + number.toString() + ', Try higher';
-                        },);
+                        setState(
+                          () {
+                            wrong = 'You tried ' + number.toString() + ', Try higher';
+                          },
+                        );
                       } else {
-                        setState(() {
-                          wrong = 'You tried ' + number.toString() + '\n' + ' You guessed right.';
-                        },);
-                        return showDialog(
-                        context: context,
-                          builder:(context)=>  AlertDialog(
-                          title: Text('You guessed right'),
-                          content: Text("It was " + number.toString()),
-                          actions: [
-                            FlatButton(
-                              textColor: Colors.black,
-                              onPressed: () {
-                                setState(() {
-                                  wrong = null;
-                                  textControler.clear();
-                                  ran = new Random();
-                                  randomNumber = ran.nextInt(100) + 1;
-                                  number = -1;
-                                },);
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('Try again'),
-                            ),
-                            FlatButton(
-                              textColor: Colors.black,
-                              onPressed: () {
-                                setState(() {
-                                  wrong = null;
-                                  button = 'Reset';
-                                  textControler.clear();
-                                  ran = new Random();
-                                  randomNumber = ran.nextInt(100) + 1;
-                                  number = -1;
-                                },);
-                                Navigator.of(context).pop();
-                              },
-                              child: Text('OK'),
-                            ),
-                          ],
-                        ),
+                        setState(
+                          () {
+                            wrong = 'You tried ' + number.toString() + '\n' + ' You guessed right.';
+                          },
+                        );
+                        return showDialog<void>(
+                          context: context,
+                          builder: (BuildContext context) => AlertDialog(
+                            title: const Text('You guessed right'),
+                            content: Text('It was ' + number.toString()),
+                            actions: <Widget>[
+                              TextButton(
+                                onPressed: () {
+                                  setState(
+                                    () {
+                                      wrong = null;
+                                      textControler.clear();
+                                      ran = Random();
+                                      randomNumber = ran.nextInt(100) + 1;
+                                      number = -1;
+                                    },
+                                  );
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('Try again'),
+                              ),
+                              TextButton(
+                                onPressed: () {
+                                  setState(
+                                    () {
+                                      wrong = null;
+                                      button = 'Reset';
+                                      textControler.clear();
+                                      ran = Random();
+                                      randomNumber = ran.nextInt(100) + 1;
+                                      number = -1;
+                                    },
+                                  );
+                                  Navigator.of(context).pop();
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          ),
                         );
                       }
                     },
-                ),
-              ],
-            ),
-          ],
-        ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
