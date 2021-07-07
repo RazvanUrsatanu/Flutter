4a81d66babff204f2591e695b03dd638580d5a21
Razvan <razvan.ursatanu@yahoo.com>
2021-07-07 13:50:27 +0300

    add analysis options

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,73 +15,70 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
+  const MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  String amount, wrong;
+  String amount = '';
+  String wrong = '';
   double res = 0;
   bool b = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Currency convertor"),
-          centerTitle: true,
-        ),
-        body: Container (
-            child: Column (
-              children: [
-                Image.network ("https://www.tellermate.com/us/wp-content/uploads/sites/8/2020/05/Cash-US-1024x512.jpg"),
-                TextField(
-                  decoration: InputDecoration (
-                      hintText: 'Enter the amount in EUR',
-                      errorText: wrong
-                  ),
-                  onChanged: (String s) {
-                    amount = s;
+      appBar: AppBar(
+        title: const Text('Currency convertor'),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: <Widget>[
+          Image.network('https://www.tellermate.com/us/wp-content/uploads/sites/8/2020/05/Cash-US-1024x512.jpg'),
+          TextField(
+              decoration: InputDecoration(hintText: 'Enter the amount in EUR', errorText: wrong),
+              onChanged: (String s) {
+                amount = s;
+              }),
+          ElevatedButton(
+              child: const Text('Convert'),
+              onPressed: () {
+                if (amount == null) {
+                  setState(
+                    () {
+                      wrong = 'please enter a number';
+                    },
+                  );
+                } else {
+                  try {
+                    setState(() {
+                      res = 4.5 * double.parse(amount);
+                      b = true;
+                      wrong = '';
+                    });
+                  } on FormatException {
+                    setState(
+                      () {
+                        wrong = 'please enter a number';
+                      },
+                    );
                   }
-                ),
-                ElevatedButton(
-                    child: Text('Convert'),
-                    onPressed: () {
-                      if (amount == null) {
-                        setState(() {
-                          wrong = 'please enter a number';
-                        },);
-                      } else {
-                        try {
-                          setState(() {
-                            res = 4.5 * double.parse(amount);
-                            b = true;
-                            wrong = null;
-                          });
-                        } on FormatException {
-                          setState(() {
-                            wrong = 'please enter a number';
-                          },);
-                        }
-                      }
-                    }
-                ),
-                Text(
-                  b == true ? res.toStringAsFixed(2) + " RON" : "",
-                  textScaleFactor: 2,
-                )
-              ],
-            ),
-        ),
+                }
+              }),
+          Text(
+            b == true ? '${res.toStringAsFixed(2)} RON' : '',
+            textScaleFactor: 2,
+          )
+        ],
+      ),
     );
   }
 }
